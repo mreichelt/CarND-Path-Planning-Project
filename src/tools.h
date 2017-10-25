2a18c660fed3361b50a1e1ecd80d70d78452a45b
Marc Reichelt <mcreichelt@gmail.com>
2017-10-26 01:32:03 +0200

    move helper methods to tools.cpp

--- src/tools.h
+++ src/tools.h
@@ -1,6 +1,10 @@
 #ifndef PATH_PLANNING_TOOLS_H
 #define PATH_PLANNING_TOOLS_H
 
+#include <vector>
+
+using namespace std;
+
 double deg2rad(double deg);
 
 double rad2deg(double rad);
@@ -9,5 +13,19 @@ double rad2deg(double rad);
  * Converts miles/hour to meters/second.
  */
 double mph2mps(double mph);
+
+double distance(double x1, double y1, double x2, double y2);
+
+size_t ClosestWaypoint(double x, double y, const vector<double> &maps_x, const vector<double> &maps_y);
+
+size_t NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y);
+
+
+// Transform from Cartesian x,y coordinates to Frenet s,d coordinates
+vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y);
+
+// Transform from Frenet s,d coordinates to Cartesian x,y
+vector<double>
+getXY(double s, double d, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y);
 
 #endif //PATH_PLANNING_TOOLS_H
